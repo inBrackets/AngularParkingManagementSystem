db4d87357b9aad7a7daa39e0d98e2c83f80b38c5
Jason <extremewinds@gmail.com>
2025-07-20 00:06:58 +0200

    reformat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,25 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {LayoutComponent} from './pages/layout/layout.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
-    path:'',
-    redirectTo:"login",
-    pathMatch:"full",
+    path: '',
+    redirectTo: "login",
+    pathMatch: "full",
   },
   {
-    path:"login",
-    component:LoginComponent,
+    path: "login",
+    component: LoginComponent,
   },
   {
-    path:'',
-    component:LayoutComponent,
-    children:[
+    path: '',
+    component: LayoutComponent,
+    children: [
       {
-        path:'dashboard',
-        component:DashboardComponent
+        path: 'dashboard',
+        component: DashboardComponent
       }
     ]
   }
