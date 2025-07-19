c964eeac7593fa63ae44b46f0fc00da9bac0ee26
Jason <extremewinds@gmail.com>
2025-07-20 01:19:59 +0200

    implement login feature with API

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {IUserModel, User} from '../../model/user.model';
+import {FormsModule} from '@angular/forms';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
+  standalone: true,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj: User = new User();
+  userSrv = inject(UserService);
 
+  onLogin() {
+    this.userSrv.loginUser(this.loginObj).subscribe((res: IUserModel) => {
+      alert("User found..navigating inside");
+    }, error => {
+      alert("Wrong Credentials");
+    })
+
+
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {IUserModel, User} from '../model/user.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(obj: User): Observable<IUserModel> {
+    return this.http.post<IUserModel>("https://api.freeprojectapi.com/api/SmartParking/login", obj)
+  }
+}
