ec75a07fa911aa5893744437b161ee239bab3ddd
Jason <extremewinds@gmail.com>
2025-07-23 21:18:29 +0200

    add new parking component

---
+++ src/app/pages/new-parking/new-parking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-parking',
+  imports: [],
+  templateUrl: './new-parking.component.html',
+  styleUrl: './new-parking.component.css'
+})
+export class NewParkingComponent {
+
+}
